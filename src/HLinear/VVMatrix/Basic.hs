36f87573075a982d00505e5cc6f4f9dfb5b3c115
Alexandru Ghitza <aghitza@alum.mit.edu>
2015-02-13 22:17:05 +1100

    fix compilation errors in VVMatrix.Basic

--- src/HLinear/VVMatrix/Basic.hs
+++ src/HLinear/VVMatrix/Basic.hs
@@ -1,13 +1,18 @@
+module HLinear.VVMatrix.Basic
+  where
+
 import qualified Data.Vector as V
+import HLinear.VVMatrix.Definition (VVMatrix(..))
 
 toLists :: VVMatrix a -> [[a]]
 toLists (VVMatrix _ _ vss) = V.toList $ V.map V.toList vss
 
 fromLists :: [[a]] -> VVMatrix a
-fromLists rs | nrs == 0  = VVMatrix 0 0 []
-             | otherwise = if any ((\=ncs) . length) $ tail rs
+fromLists rs | nrs == 0  = VVMatrix 0 0 V.empty
+             | otherwise = if any ((/=ncs) . length) $ tail rs
                            then error "rows must have the same length"
-                           else VVMatrix nrs ncs $ V.toList (map V.toList rs)
+                           else VVMatrix nrs ncs $ V.fromList (map V.fromList rs)
   where
-    nrs = length nrs
+    nrs = length rs
     ncs = length $ head rs
+
