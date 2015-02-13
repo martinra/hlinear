5c1e143f09045398ca6679abc2d1b03370e14ad3
Alexandru Ghitza <aghitza@alum.mit.edu>
2015-02-13 22:02:56 +1100

    fix duplication mistake

--- src/HLinear/VVMatrix.hs
+++ src/HLinear/VVMatrix.hs
@@ -2,11 +2,6 @@ module HLinear.VVMatrix
 
 where
 
-import HLinear.Basic ( toLists
-                     , fromLists )
-
-import HLinear.Definition ( VVMatrix(..) )
-
 import HLinear.VVMatrix.Basic ( toLists
                               , fromLists )
 
