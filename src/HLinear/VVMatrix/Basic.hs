6b85d26db4d94b21b6181683be32d29da54ccaba
Martin Raum <martin@raum-brothers.eu>
2015-02-17 13:31:45 +0100

    Add Show VVMatrix

--- src/HLinear/VVMatrix/Basic.hs
+++ src/HLinear/VVMatrix/Basic.hs
@@ -19,3 +19,19 @@ fromLists rs | nrs == 0  = VVMatrix 0 0 V.empty
 instance Eq a => Eq (VVMatrix a) where
   (VVMatrix nrs ncs rs) == (VVMatrix nrs' ncs' rs') =
     nrs == nrs' && ncs == ncs' && rs == rs'
+
+instance Show a => Show (VVMatrix a) where
+  show (VVMatrix 0 _ rs) = "[ ]"
+  show (VVMatrix _ 0 rs) = "[ ]"
+  show (VVMatrix _ _ rs) =
+    V.foldl1 (\r r' -> r ++ "\n" ++ r') $ V.map show' rsShown
+    where
+    rsShown = V.map (V.map show) rs
+    show' r= "[ " ++ rShown ++ " ]"
+      where
+      rShown = V.foldl1 (\a a' -> a ++ " " ++ a') $ V.map center r
+    center s = replicate n ' ' ++ s ++ replicate n' ' '
+      where
+      maxLength = V.maximum $ V.map (V.maximum . V.map length) rsShown
+      n = (maxLength - length s) `div` 2
+      n' = maxLength - n - length s
