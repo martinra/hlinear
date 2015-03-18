43d166e9a872f2bb0d5ab08f4ec12338946084db
Martin Raum <martin@raum-brothers.eu>
2015-03-18 08:38:25 +0100

    Add Zero One and Sized type

--- src/HLinear/VVMatrix/Definition.hs
+++ src/HLinear/VVMatrix/Definition.hs
@@ -1,7 +1,21 @@
+{-# LANGUAGE 
+    DataKinds
+  , KindSignatures 
+  #-}
+
 module HLinear.VVMatrix.Definition
 where
 
 import Data.Vector ( Vector )
+import GHC.TypeLits ( Nat )
 
 
-data VVMatrix a = VVMatrix Int Int (Vector (Vector a))
+-- | VVMatrix models the union of matrices of all sizes with entries in a
+--   Zero and One elements, which don't have distinguished size are separate
+--   constructurs
+data VVMatrix a =
+    Zero
+  | One
+  | VVMatrix !Int !Int (Vector (Vector a))
+
+newtype SizedVVMatrix (m :: Nat) (n :: Nat) a = SizedVVMatrix (VVMatrix a)
