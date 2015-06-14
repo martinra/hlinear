0267171384744115257086712db19b25034dadea
Martin Raum <martin@raum-brothers.eu>
2015-06-14 22:43:00 +0200

    Test splitOffPLEHook

--- test/HLinear/Test/PLE/Hook.hs
+++ test/HLinear/Test/PLE/Hook.hs
@@ -5,11 +5,12 @@ import Test.Tasty
 
 import HLinear.Test.PLE.Hook.EchelonForm
 import HLinear.Test.PLE.Hook.LeftTransformation
-
+import HLinear.Test.PLE.Hook.PLE
 
 hookTests :: TestTree
 hookTests =
   testGroup "PLEHook Tests"
   [-- leftTransformationTests
-    echelonFormProperties
+   -- echelonFormProperties
+   hookPLEProperties
   ]
---
+++ test/HLinear/Test/PLE/Hook/PLE.hs
@@ -0,0 +1,47 @@
+module HLinear.Test.PLE.Hook.PLE
+where
+
+import qualified Prelude as P
+import Prelude hiding ( (+), (-), negate, subtract
+                      , (*), (/), recip, (^), (^^)
+                      , gcd
+                      , quotRem, quot, rem
+                      )
+
+import Data.Maybe
+import Data.Proxy
+import Math.Structure
+import Math.Structure.Tasty
+
+import Test.Tasty
+import Test.Tasty.QuickCheck as QC
+import Test.Vector
+
+import HLinear.Matrix as M
+import HLinear.PLE.Hook.EchelonForm as EF
+import HLinear.PLE.Hook.LeftTransformation as LT
+import HLinear.PLE.Hook.RPermute as RP
+import HLinear.PLE.PLE
+import HLinear.PLE.Hook.PLE as PLE
+
+import HLinear.Test.Utils
+
+import Debug.Trace
+
+hookPLEProperties :: TestTree
+hookPLEProperties =
+  testGroup "Hook PLE"
+  [ QC.testProperty "recombine splitOffHook" $
+      \m -> let _ = m :: Matrix Rational
+            in fromMaybe True $ do
+                 (PLEHook p l e, m') <- splitOffHook m
+                 let m'zero = if LT.nmbCols l == 1
+                              then M.zeroMatrix 1 1 `mappend` m'
+                              else M.zeroMatrix (M.nmbRows m) 1 `M.blockSumRows` m'
+                 let pm = RP.toMatrix p
+                 let lm = LT.toMatrix l
+                 let em = EF.toMatrix e
+                 let d (Matrix nrs ncs _) = show [nrs,ncs]
+                 return $ lm * pm * m == m'zero + em
+
+  ]
