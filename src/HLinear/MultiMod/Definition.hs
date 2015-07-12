9f1890922420402bfc190e7ca7e414cd5d733371
Martin Westerholt-Raum <martin@raum-brothers.eu>
2015-07-12 23:47:23 +0200

    Stub modimod matrices.

--- src/HLinear/MultiMod/Definition.hs
+++ src/HLinear/MultiMod/Definition.hs
@@ -1,83 +1,29 @@
 {-# LANGUAGE
-    FunctionalDependencies
+    FlexibleContexts
+  , MultiParamTypeClasses
   , RankNTypes
   #-}
 
 module HLinear.MultiMod.Definition
 where
 
-import HFlint.FMPQ ( FMPQ )
-import qualified HFlint.FMPQ as FMPQ
-import Data.List.Split ( splitEvery )
-import Data.Traversable ( for )
-import Control.Parallel.Strategies
+import Data.Proxy
+import qualified Data.Vector as V
+import HFlint.FMPZ
+import HFlint.NMod ( NMod, ReifiesNModContext
+                   , Modulus(..)
+                   )
 
 
--- -- a approximates b to quality q
--- class Approximates a b q | a -> b, a -> q where
---   approximate :: b -> a
---   increaseQuality :: a -> a
---   reconstruct :: (b -> q -> Bool) -> a -> b
+data MultiMod f =
+  MultiMod { unMultiMod
+               :: forall ctx .
+                  ReifiesNModContext ctx
+               => Proxy ctx -> Maybe (f (NMod ctx))
+           }
 
+class Reducible f a where
+  reduce :: f a -> MultiMod f
 
--- newtype MultiMod f where
---   MultiMod :: ReifiesNModCtx ctx => f (NMod ctx) -> MultiMod f
--- 
--- --class MultiModReducible Matrix where
--- --class MultiModReconstructible (PLREDecomposition FMPQ)  where
--- --newtype PLREDecomposition = Permutation LeftTransformation EchelonReduction ReducedEchelonForm
--- 
--- reduce :: ReifiesNModCtx ctx => Matrix FMPQ -> ctx -> Maybe (Matrix (NMod ctx))
--- reduce ctx m@(Matrix nrs ncs rs) = Matrix nrs ncs <$>
---   V.sequence ( V.map (V.sequence . V.map (FMPQ.reduce ctx)) rs )
--- 
--- initialPrime :: Word64
--- initialPrime = nextPrime 2^60
--- 
--- newtype MultiModMatrix = MultiModMatrix
---   { nmbRows :: Natural
---   , nmbCols :: Natural
---   , reduction :: ReifiesNModCtx ctx => Maybe (Matrix (NMod ctx))
---   }
--- 
--- newtype MultiModLeftTransformation = MultiModLeftTransformation
---   { reduction :: ReifiesNModCtx ctx => Maybe (LeftTransformation (NMod ctx))
---   }
--- 
--- reduce :: Matrix FMPQ -> MultiModMatrix 
--- reduce (Matrix nrs ncs rs) = MultiModMatrix nrs ncs $
---   Matrix nrs ncs <$> V.sequence ( V.map (V.sequence . V.map FMPQ.reduce) rs )
--- 
--- instance HasPLE MultiModMatrix where
---   type PLEPermute MultiModMatrix = RPermute
---   type PLELeft MultiModMatrix    = MultiModLeftTransformation
---   type PLEEchelon MultiModMatrix = MultiModEchelonForm
---   
--- 
--- 
--- reconstruct :: ( FMPZ -> Matrix FMPQ -> Bool ) -> MultiModMatrixFMPQ -> FMPQ
--- reconstruct isFinal (MultiModMatrix nrs ncs red) =
---    head $ dropUntil (uncurry isFinal) $
---    scanl reconstruct (0, zeroMatrix nrs ncs) modularPLEs
---   where
---     reconstruct (mod, m) 
--- 
--- 
---     modularPLEs = ( map catMaybes $ splitEvery chunkSize $
---                     for primes $ \p -> (p,) <$> withNModContext p
---                                               (liftA ple . red)
---                   ) `using` evalList (parList rseq)
---     chunkSize = 20
---     primes = map fromInteger $ iterate nextPrime initialPrime
--- 
--- 
--- instance Approximates MultiModMatrixQQ (Matrix FMPQ) FMPZ where
---   approximate m@(Matrix nrs ncs _) = MultiModMatrixQQ
---     { reductions     = reduceModulo m
---     , reconstruction = return $ zeroMatrix nrs ncs
---     }
---   reconstruct isFinal mm = runIdentity $
---     evalStateT 0 $
---     evalState (FMPZ.nextPrime $ fromInteger 2^60) $
---     reconstructionS
---     
+class Reconstructible f a where
+  reconstruct :: (Modulus FMPZ -> f a -> Bool) -> MultiMod f -> f a
---
+++ src/HLinear/MultiMod/Matrix.hs
@@ -0,0 +1,100 @@
+{-# LANGUAGE
+    FlexibleContexts
+  , FlexibleInstances
+  , MultiParamTypeClasses
+  , RankNTypes
+  , ScopedTypeVariables
+  , TupleSections
+  #-}
+
+module HLinear.MultiMod.Matrix
+where
+
+import Prelude hiding ( (+), (-), negate, subtract
+                      , (*), (/), recip, (^), (^^)
+                      , gcd
+                      , quotRem, quot, rem
+                      )
+import qualified Prelude as P
+
+import Control.Applicative ( liftA2 )
+import Control.Parallel.Strategies ( using, rpar, parListChunk )
+import Control.DeepSeq ( NFData, rnf )
+import Data.Composition ( (.:) )
+import Data.Proxy
+import Data.Maybe
+import Data.Word ( Word64 )
+import qualified Data.Vector as V
+import HFlint.FMPQ
+import HFlint.FMPZ as FMPZ
+import HFlint.NMod
+import HFlint.Primes ( primesAfter )
+import qualified HFlint.NMod as NMod
+import Math.Structure
+
+import HLinear.MultiMod.Definition
+import HLinear.Matrix ( Matrix(..) )
+import qualified HLinear.Matrix as M
+
+import Debug.Trace ( trace )
+
+
+instance HasLimbHeight a => HasLimbHeight (Matrix a) where
+  limbHeight (Matrix _ _ rs) =
+    V.maximum $ V.map (V.maximum . V.map limbHeight) rs
+
+instance Reducible Matrix FMPQ where
+  reduce m = MultiMod $ \_ -> M.mapM toNModMay m
+
+
+type Approx f = Maybe (Modulus FMPZ, f FMPZ)
+type ApproxRefiner f = Approx f -> Approx f
+
+rnfApproxRefiner :: MultiMod Matrix -> Word64 -> ApproxRefiner Matrix
+rnfApproxRefiner (MultiMod m) p =
+  withNModContext (fromIntegral p) $ \proxy ->
+    let m' = m proxy
+    in seq (rnf m') $ approxRefiner m'
+
+approxRefiner
+  :: forall ctx
+  .  ReifiesNModContext ctx
+  => Maybe (Matrix (NMod ctx))
+  -> ApproxRefiner Matrix
+approxRefiner mPt' approx =
+  mPt' >>= \mPt -> Just $
+    let modulusPt@(Modulus modulusPtZ) = NMod.modulusIntegral (Proxy :: Proxy ctx)
+    in case approx of
+         Nothing ->
+           ( modulusPt
+             -- todo: specialize fromIntegral :: NMod -> FMPZ
+           , M.map FMPZ.fromNMod mPt
+           )
+         Just ( modulus@(Modulus modulusZ), m ) ->
+           ( Modulus $ modulusPtZ * modulusZ
+           , M.zipWith (chineseRemainder modulus) m mPt
+           )
+
+instance Reconstructible Matrix FMPQ where
+  reconstruct isReconstruction m = mQ
+    where
+    (_,mQ) = head $ dropWhile ( not . uncurry isReconstruction ) $ rationalReconstructions
+    rationalReconstructions = catMaybes $ flip map chinesesRemainders $ \(modulus, mat) ->
+      (modulus,) <$> M.mapM (rationalReconstruct Balanced modulus) mat
+    chinesesRemainders = catMaybes $ scanl (trace "here" $ flip ($)) Nothing refiners
+    refiners = map (rnfApproxRefiner m) (primesAfter 0XDFFFFFFFFFFFFFFF)
+-- fixme: this hangs
+--               `using` (parListChunk 20 rpar)
+
+mulMultiMod :: Matrix FMPQ -> Matrix FMPQ -> Matrix FMPQ
+mulMultiMod m m' = reconstruct isReconstruction $ mmod * mmod'
+  where
+    ht = limbHeight m
+    ht' = limbHeight m'
+    mmod = reduce m
+    mmod' = reduce m'
+    isReconstruction modulus _ = trace ( show (limbHeight modulus) ++ show ( 1 P.+ ht P.+ ht' ) ) $ limbHeight modulus > 1 P.+ ht P.+ ht'
+
+instance MultiplicativeMagma (MultiMod Matrix) where
+  (MultiMod m) * (MultiMod m') = MultiMod $ \proxy ->
+      liftA2 (*) (m proxy) (m' proxy)
