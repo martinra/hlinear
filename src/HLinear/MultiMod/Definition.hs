d28c7dc2dcebb0d1aee99c988bdb37b61f7de8b7
Martin Westerholt-Raum <martin@raum-brothers.eu>
2015-07-18 07:46:15 +0200

    Abstract MultiMod for Matrix

--- src/HLinear/MultiMod/Definition.hs
+++ src/HLinear/MultiMod/Definition.hs
@@ -1,26 +1,75 @@
 {-# LANGUAGE
-    FlexibleContexts
+    ExistentialQuantification
+  , FlexibleContexts
+  , FlexibleInstances
+  , FunctionalDependencies
   , MultiParamTypeClasses
   , RankNTypes
+  , ScopedTypeVariables
   #-}
 
 module HLinear.MultiMod.Definition
 where
 
+import Control.Parallel.Strategies ( using, rseq, parBuffer )
+import Data.Bits ( shiftR )
+import Data.Maybe
 import Data.Proxy
 import qualified Data.Vector as V
 import HFlint.FMPZ
 import HFlint.NMod ( NMod, ReifiesNModContext
                    , Modulus(..)
+                   , FlintLimb
                    )
+import HFlint.Primes ( primesAfter )
 
 
 data MultiMod f =
   MultiMod { unMultiMod
-               :: forall ctx .
-                  ReifiesNModContext ctx
+               :: forall ctx
+               .  ReifiesNModContext ctx
                => Proxy ctx -> Maybe (f (NMod ctx))
            }
 
 class Reducible f a where
   reduce :: f a -> MultiMod f
+
+data Mod f a = Mod (Modulus FMPZ) (f a)
+data RReconst f d a = RReconst d (Modulus FMPZ) (f a)
+
+data Approximation f d a =
+    ApproxInvalid
+  | forall ctx . ReifiesNModContext ctx => ApproxPrimitive d (f (NMod ctx))
+  | ApproxCombined d (Mod f a)
+
+class Approximable f d where
+  toApprox :: MultiMod f -> FlintLimb -> Approximation f d FMPZ
+  approxAppend :: Approximation f d FMPZ -> Approximation f d FMPZ -> Approximation f d FMPZ
+
+instance Approximable f d => Monoid (Approximation f d FMPZ) where
+  mempty = ApproxInvalid
+  mappend = approxAppend
+
+class Approximable f d => Reconstructible f d a | f a -> d where
+  fromApprox :: Approximation f d FMPZ -> Maybe (RReconst f d a)
+
+reconstruct :: Reconstructible f d a => (RReconst f d a -> Bool) -> MultiMod f -> f a
+reconstruct isReconstruction mf = fa
+  where
+  RReconst _ _ fa = head $ dropWhile ( not . isReconstruction ) $
+                    catMaybes rationalReconstructions
+  rationalReconstructions =  map fromApprox $ takeEveryNth rrSteps $
+                             scanl1 mappend approximations
+  approximations =
+     map (toApprox mf) (primesAfter $ (maxBound :: FlintLimb) `shiftR` 2)
+     `using` parBuffer bufferSize rseq
+  rrSteps = 20
+  bufferSize = 3 * rrSteps
+
+takeEveryNth :: Int -> [a] -> [a]
+takeEveryNth n = takeEveryNth' (pred n)
+  where
+  takeEveryNth' n' as =
+    case drop n' as of
+      (a':as') -> a':takeEveryNth' n' as'
+      []       -> []
--- src/HLinear/MultiMod/Matrix.hs
+++ src/HLinear/MultiMod/Matrix.hs
@@ -19,9 +19,7 @@ import Prelude hiding ( (+), (-), negate, subtract
 import qualified Prelude as P
 
 import Control.Applicative ( liftA2 )
-import Control.Parallel.Strategies ( using, rseq, parBuffer )
 import Control.DeepSeq ( NFData, rnf )
-import Data.Bits ( shiftR )
 import Data.Composition ( (.:) )
 import Data.Proxy
 import Data.Reflection
@@ -31,7 +29,6 @@ import qualified Data.Vector as V
 import HFlint.FMPQ
 import HFlint.FMPZ as FMPZ
 import HFlint.NMod
-import HFlint.Primes ( primesAfter )
 import qualified HFlint.NMod as NMod
 import Math.Structure
 import Numeric.Natural
@@ -39,8 +36,6 @@ import Numeric.Natural
 import HLinear.MultiMod.Definition
 import HLinear.Matrix ( Matrix(..) )
 import qualified HLinear.Matrix as M
-
-import Debug.Trace ( trace )
 
 
 instance HasLimbHeight a => HasLimbHeight (Matrix a) where
@@ -50,93 +45,57 @@ instance HasLimbHeight a => HasLimbHeight (Matrix a) where
 instance Reducible Matrix FMPQ where
   reduce m = MultiMod $ \_ -> M.mapM toNModMay m
 
+fromApproxPrimitivetoCombined
+  :: Approximation Matrix () FMPZ -> Approximation Matrix () FMPZ
+fromApproxPrimitivetoCombined (ApproxPrimitive () (m :: Matrix (NMod ctx))) =
+  let md = NMod.modulusIntegral (Proxy :: Proxy ctx)
+  in ApproxCombined () $ Mod md $ M.map FMPZ.fromNMod m
+fromApproxPrimitivetoCombined a = a
 
-data ModMatrix a = ModMatrix (Modulus a) (Matrix a)
-data ApproxMatrix a =
-    TrivialApprox
-  | ApproxProper (ModMatrix a)
-  | ApproxRefinement Natural Natural (ModMatrix a -> ModMatrix a)
+instance Approximable Matrix () where
+  toApprox (MultiMod m) p =
+    withNModContext (fromIntegral p) $ \proxy ->
+      let m' = m proxy
+      in seq (rnf m') $
+         case m' of 
+           Nothing  -> ApproxInvalid
+           Just m'' -> ApproxPrimitive () m''
 
--- RR RationalReconstruction
-data RRMatrix = RRMatrix (Modulus FMPZ) (Matrix FMPQ)
-
-fromApprox :: ApproxMatrix FMPZ -> Maybe RRMatrix
-fromApprox TrivialApprox = Nothing
-fromApprox (ApproxProper (ModMatrix modulus m)) = RRMatrix modulus <$>
-  M.mapM (rationalReconstruct Balanced modulus) m
-fromApprox (ApproxRefinement nrs ncs ref) =
-  let ModMatrix modulus m = ref $ ModMatrix (Modulus 1) $ M.zeroMatrix nrs ncs
-  in  RRMatrix modulus <$>
-        M.mapM (rationalReconstruct Balanced modulus) m
-
-
-instance Monoid (ApproxMatrix FMPZ) where
-  mempty = TrivialApprox
-
-  mappend TrivialApprox a = a
-  mappend a TrivialApprox = a
-  mappend (ApproxProper (ModMatrix md m))
-          (ApproxProper (ModMatrix md' m')) =
-    ApproxProper $ ModMatrix (md*md') $
-      withFMPZCRTContext md md' $ \(_ :: Proxy ctx) ->
-      M.map unFMPZCRT ( M.zipWith chineseRemainder m m' :: Matrix (FMPZCRT ctx) )
-  mappend (ApproxProper m) (ApproxRefinement _ _ ref) = ApproxProper $ ref m
-  mappend (ApproxRefinement _ _ ref) (ApproxProper m) = ApproxProper $ ref m
-  mappend (ApproxRefinement nrs ncs ref) (ApproxRefinement nrs' ncs' ref') =
-    ApproxRefinement (max nrs nrs') (max ncs ncs') $ ref' . ref
-  
-
-
-toApproxRNF :: MultiMod Matrix -> FlintLimb -> ApproxMatrix FMPZ
-toApproxRNF (MultiMod m) p = 
-  withNModContext (fromIntegral p) $ \proxy ->
-    let m' = m proxy
-    in seq (rnf m') $ toApprox' m'
-
-toApprox'
-  :: forall ctx
-  .  ReifiesNModContext ctx
-  => Maybe (Matrix (NMod ctx))
-  -> ApproxMatrix FMPZ
-toApprox' Nothing = TrivialApprox
-toApprox' (Just m') = ApproxRefinement (nmbRows m') (nmbCols m') $
-                        \(ModMatrix modulus@(Modulus md) m) ->
-  let ctxProxy = Proxy :: Proxy ctx
-      Modulus md' = NMod.modulusIntegral ctxProxy
-  in  ModMatrix
-        ( Modulus $ md * md' )
-        ( withFMPZCRTNModContext modulus ctxProxy $ \(_ :: Proxy ctx') ->
+  approxAppend ApproxInvalid a = a
+  approxAppend a ApproxInvalid = a
+  approxAppend
+      (ApproxCombined _ (Mod md m))
+      (ApproxPrimitive _ (m' :: Matrix (NMod ctx))) =
+    let ctxProxy = Proxy :: Proxy ctx
+        md' = NMod.modulusIntegral ctxProxy
+    in  ApproxCombined () $ Mod (md * md') $
+          withFMPZCRTNModContext md ctxProxy $ \(_ :: Proxy ctx') ->
             let mCRT = M.zipWith chineseRemainder m m' :: Matrix (FMPZCRTNMod ctx')
             in  M.map unFMPZCRTNMod mCRT
-        )
+  approxAppend (ApproxCombined _ (Mod md m)) (ApproxCombined _ (Mod md' m')) =
+    ApproxCombined () $ Mod (md * md') $
+      withFMPZCRTContext md md' $ \(_ :: Proxy ctx) ->
+      M.map unFMPZCRT ( M.zipWith chineseRemainder m m' :: Matrix (FMPZCRT ctx) )
+  approxAppend m@(ApproxPrimitive () _) m'@(ApproxPrimitive () _) =
+    approxAppend (fromApproxPrimitivetoCombined m) m'
+  approxAppend m m' = approxAppend m' m
 
-reconstruct :: (RRMatrix -> Bool) -> MultiMod Matrix -> Matrix FMPQ
-reconstruct isReconstruction m = mQ
-  where
-  RRMatrix _ mQ = head $ dropWhile ( not . isReconstruction ) $ catMaybes rationalReconstructions
-  rationalReconstructions =  map fromApprox $ takeEveryNth rrSteps $ scanl1 mappend approximations
-  approximations =
-     map (toApproxRNF m) (primesAfter $ (maxBound :: FlintLimb) `shiftR` 2)
-     `using` parBuffer bufferSize rseq
-  rrSteps = 20
-  bufferSize = 3 * rrSteps
+instance Reconstructible Matrix () FMPQ where
+  fromApprox ApproxInvalid = Nothing
+  fromApprox m@(ApproxPrimitive () _) = fromApprox $ fromApproxPrimitivetoCombined m
+  fromApprox (ApproxCombined () (Mod modulus m)) =
+    RReconst () modulus <$>
+    M.mapM (rationalReconstruct Balanced modulus) m
 
-takeEveryNth :: Int -> [a] -> [a]
-takeEveryNth n = takeEveryNth' (pred n)
-  where
-  takeEveryNth' n' as =
-    case drop n' as of
-      (a':as') -> a':takeEveryNth' n' as'
-      []       -> []
 
 mulMultiMod :: Matrix FMPQ -> Matrix FMPQ -> Matrix FMPQ
 mulMultiMod m m' = reconstruct isReconstruction $ mmod * mmod'
   where
+    isReconstruction (RReconst () modulus _) = limbHeight modulus > 1 P.+ ht P.+ ht'
     ht = limbHeight m
     ht' = limbHeight m'
     mmod = reduce m
     mmod' = reduce m'
-    isReconstruction (RRMatrix modulus _) = limbHeight modulus > 1 P.+ ht P.+ ht'
 
 instance MultiplicativeMagma (MultiMod Matrix) where
   (MultiMod m) * (MultiMod m') = MultiMod $ \proxy ->
