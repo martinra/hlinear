7638b95e095c04e42e37161b65a411166dba2f0e
Martin Westerholt-Raum <martin@raum-brothers.eu>
2015-08-10 11:09:16 +0200

    correct sliced PLE

--- src/HLinear/PLE/Sliced/Echelonize/DivisionRing.hs
+++ src/HLinear/PLE/Sliced/Echelonize/DivisionRing.hs
@@ -15,7 +15,7 @@ import Prelude hiding ( (+), (-), negate, subtract
                       , quotRem, quot, rem
                       )
 
-import Control.Arrow ( (***), second )
+import Control.Arrow ( (***), first, second )
 import Control.Monad.Identity
 import Control.Monad.Par
 import Data.Vector ( Vector )
@@ -30,6 +30,7 @@ import HLinear.PLE.Decomposition.Definition
 import HLinear.PLE.FoldUnfold.Echelonize ( firstHook )
 import HLinear.PLE.Hook.Definition
 import HLinear.PLE.Hook.PLMatrix
+import qualified HLinear.PLE.Hook.RPermute as RP
 import qualified HLinear.PLE.Hook.EchelonForm as EF
 import HLinear.PLE.Sliced.Echelonize.Definition
 import HLinear.PLE.Strategy.Definition
@@ -37,32 +38,99 @@ import HLinear.PLE.Strategy.Definition
 
 slicingPositions
   :: PLEDecompositionSlicedParameters
-  -> Int -> Vector (Int, Int)
-slicingPositions param ncs =
-  bigSlices V.++ smallSlices
-  where
-   nmbSlices :: Int
-   nmbSlices = case slicingSizeNmb param of
-     SlicingSize paramSz ->
-       let paramSzZ = fromIntegral paramSz
-       in  if ncs `mod` paramSzZ == 0
-           then ncs `div` paramSzZ
-           else 1 + ncs `div` paramSzZ
-     SlicingNmb paramNmb -> fromIntegral paramNmb
-        
-   nmbSmallSlice = ncs `mod` nmbSlices
-   nmbBigSlice = nmbSlices - nmbSmallSlice
-   sizeBigSlice =
-      if nmbSmallSlice == 0
-      then ncs `div` nmbSlices
-      else 1 + ncs `div` nmbSlices
-   sizeSmallSlice = sizeBigSlice - 1
+  -> Natural -> Vector (Int, Int)
+slicingPositions _ 0 = V.empty
 
-   bigSlices = V.iterateN nmbBigSlice
-                 (second (+sizeBigSlice)) (0,0)
-   smallSlices = V.iterateN nmbSmallSlice
-                 (second (+sizeSmallSlice))
-                 (V.last bigSlices)
+slicingPositions
+  ( PLEDecompositionSlicedParameters
+    SlicingUnBalanced paramSizeNmb
+  ) ncs
+  = bigSlices V.++ smallSlices
+  where
+    ncsZ = fromIntegral ncs
+
+    nmbBigSlices = fromIntegral $
+      case paramSizeNmb of
+        SlicingSize paramSz -> ncs `div` paramSz
+        SlicingNmb paramNmb -> paramNmb
+
+    sizeBigSlice = fromIntegral $
+      case paramSizeNmb of
+        SlicingSize paramSz -> paramSz
+        SlicingNmb paramNmb -> ncs `div` paramNmb
+
+    sizeSmallSlice = fromIntegral $
+      case paramSizeNmb of
+        SlicingSize paramSz -> ncs `mod` paramSz
+        SlicingNmb paramNmb -> ncs `mod` paramNmb
+
+    bigSlices = if sizeBigSlice == 0
+                then V.empty
+                else V.iterateN nmbBigSlices
+                       (first (+sizeBigSlice)) (0,sizeBigSlice)
+    smallSlices = if sizeSmallSlice == 0
+                  then V.empty
+                  else V.singleton ( sizeBigSlice * nmbBigSlices
+                                   , sizeSmallSlice )
+
+slicingPositions
+  ( PLEDecompositionSlicedParameters
+    SlicingBalanced (SlicingSize paramSz)
+  ) ncs
+  = if remainder == 0
+    then normalSlices
+    else normalSlices
+         `V.snoc`
+         ( ncsZ - remainder, remainder )
+  where
+    ncsZ = fromIntegral ncs
+    sz = fromIntegral paramSz
+
+    (n,r) = ncsZ `divMod` sz
+
+    (nmbBigSlices, nmbSmallSlices, remainder) =
+      if sz-1 - r > n
+      then (n,0,r)
+      else (n - (sz-1) + r, 1 + sz-1 - r, 0)
+    nmbBothSlices = nmbBigSlices + nmbSmallSlices
+
+    normalSlices =
+      if nmbBothSlices == 0
+      then V.empty
+      else slicingPositions
+             ( PLEDecompositionSlicedParameters
+               SlicingBalanced (SlicingNmb $ fromIntegral nmbBothSlices)
+             )
+             ( fromIntegral $ ncsZ - remainder )
+
+
+slicingPositions
+  ( PLEDecompositionSlicedParameters
+    SlicingBalanced (SlicingNmb nmbSlices)
+  ) ncs
+  = bigSlices V.++ smallSlices
+  where
+    nmbSlicesZ = fromIntegral nmbSlices
+
+    nmbBigSlices = fromIntegral $ ncs `mod` nmbSlices
+    nmbSmallSlices = nmbSlicesZ - nmbBigSlices
+
+    sizeSmallSlice = fromIntegral $ ncs `div` nmbSlices
+    sizeBigSlice = succ sizeSmallSlice
+
+    bigSlices =
+      if sizeBigSlice == 0
+      then V.empty
+      else V.iterateN nmbBigSlices
+             (first (+sizeBigSlice)) (0,sizeBigSlice)
+    smallSlices =
+      if sizeSmallSlice == 0
+      then V.empty
+      else V.iterateN nmbSmallSlices
+             ( first (+sizeSmallSlice) )
+             ( nmbBigSlices * sizeBigSlice
+             , sizeSmallSlice
+             )
   
 
 instance
@@ -72,7 +140,7 @@ instance
   where
   pleDecompositionSliced parameters strat m = runPar $ do
     ivhook <- spawnP $ firstHook m
-    ivms <- for (slicingPositions parameters $ fromIntegral $ M.nmbCols m) $ \(ix,s) ->
+    ivms <- for (slicingPositions parameters $ M.nmbCols m) $ \(ix,s) ->
               spawnP $ M.sliceCols ix s m
     pleDecompositionSlicedPar strat ivhook ivms
 
@@ -88,7 +156,7 @@ pleDecompositionSlicedPar strat ivhook ivms_
   | otherwise = do
       let ivm = V.head ivms_
       let ivms = V.tail ivms_
-    
+
       ivple <- spawn $ unPLEDecomposition <$> runIdentity <$>
                  dispatchPLEStrategy strat <$> get ivm
       PLEHook p l e <- get ivple
@@ -100,10 +168,10 @@ pleDecompositionSlicedPar strat ivhook ivms_
           m <- get ivm
           r <- get ivr
           let (ivem, ivm') = spawnP *** spawnP $ M.splitAtCols r $
-                               fromPLMatrix $ l*.(p*. PLMatrix m)
+                               fromPLMatrix $ l *. (p *. PLMatrix m)
           (,) <$> ivem <*> ivm'
-    
+
       ive' <- spawn $ V.foldl EF.blockSum e <$> traverse get ivems
       ivhook' <- spawn $ (*) <$> get ivhook <*> (PLEHook p l <$> get ive')
-    
+
       pleDecompositionSlicedPar strat ivhook' ivms'
