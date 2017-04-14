32eb0c3f1b63dc956f902678ccfec3f3134f5192
Martin Raum <martin@raum-brothers.eu>
2017-04-14 13:11:55 +0200

    reformulate echelon form addition

--- src/HLinear/PLE/Hook/EchelonForm/Algebra.hs
+++ src/HLinear/PLE/Hook/EchelonForm/Algebra.hs
@@ -21,19 +21,21 @@ import HLinear.PLE.Hook.EchelonForm.Row as EFR
 -- This can lead to inconsitent data structures. We assume that
 -- the number of columns in the first argument equals the number of columns in
 -- the second one + the offset of the first argument
+-- todo: we can obtain a consistend addition by sorting rows, if necessary
 --------------------------------------------------------------------------------
 
 instance AdditiveMagma a => AdditiveMagma (EchelonForm a) where
   e@(EchelonForm nrs ncs rs) + e'@(EchelonForm nrs' ncs' rs')
-    | lrs == 0 = ef (extendRows maxnrsZ minbrs nrs'Z brs' srs')
+    | lrs  == 0 = ef (extendRows maxnrsZ minbrs nrs'Z brs' srs')
     | lrs' == 0 = ef (extendRows maxnrsZ minbrs nrsZ brs srs)
-    -- this is the case that occur in the PLE decomposition
-    | brs >= nrs'Z =
-        let zeros = V.replicate (brs - nrs'Z) (EFR.zero maxncs)
-        in  ef $ srs V.++ zeros V.++ srs'
-    | otherwise = ef $ V.zipWith (+)
-        (extendRows maxnrsZ minbrs nrsZ brs srs)
-        (extendRows maxnrsZ minbrs nrs'Z brs' srs')
+    | otherwise = ef $ case compare brs nrs'Z of
+       -- the first one is the case that occurs in the PLE decomposition
+       EQ -> srs `mappend` srs'
+       GT -> let zeros = V.replicate (brs - nrs'Z) (EFR.zero maxncs)
+             in  srs `mappend` zeros `mappend` srs'
+       LT -> V.zipWith (+)
+              (extendRows maxnrsZ minbrs nrsZ brs srs)
+              (extendRows maxnrsZ minbrs nrs'Z brs' srs')
     where
       lrs = V.length rs
       lrs' = V.length rs'
@@ -55,9 +57,9 @@ instance AdditiveMagma a => AdditiveMagma (EchelonForm a) where
       ef = EchelonForm maxnrs maxncs
 
       extendRows mt mb t b rs =
-         V.replicate (mt - t) (EFR.zero maxncs)
-         V.++ rs V.++
-         V.replicate (b - mb) (EFR.zero maxncs)
+        V.replicate (mt - t) (EFR.zero maxncs)
+        V.++ rs V.++
+        V.replicate (b - mb) (EFR.zero maxncs)
 
 instance AdditiveSemigroup a => AdditiveSemigroup (EchelonForm a)
 
