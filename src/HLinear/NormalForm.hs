cd29b551a49b2d260c43eee6a18ed89b7315e89e
Martin Raum <martin@raum-brothers.eu>
2017-05-06 11:48:35 +0200

    correctly export normal forms

--- src/HLinear/NormalForm.hs
+++ src/HLinear/NormalForm.hs
@@ -1,9 +1,11 @@
 module HLinear.NormalForm
+  ( module Import
+  )
 where
 
-import HLinear.NormalForm.PLE ( HasPLE(..) )
-import HLinear.NormalForm.PLH ( HasPLH(..) )
-import HLinear.NormalForm.RREF ( HasRREF(..) )
+import HLinear.NormalForm.PLE as Import ( HasPLE(..) )
+import HLinear.NormalForm.PLH as Import ( HasPLH(..) )
+import HLinear.NormalForm.RREF as Import ( HasRREF(..) )
 import HLinear.NormalForm.Instance.PLE ()
 import HLinear.NormalForm.Instance.PLH ()
 import HLinear.NormalForm.Instance.RREF ()
