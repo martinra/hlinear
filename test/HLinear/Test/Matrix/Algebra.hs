316e99c9f75aa30141e89d717e363ec306021c02
Martin Raum <martin@raum-brothers.eu>
2017-07-04 17:39:57 +0200

    correct Matrix.Algebra test

--- test/HLinear/Test/Matrix/Algebra.hs
+++ test/HLinear/Test/Matrix/Algebra.hs
@@ -5,26 +5,21 @@
 module HLinear.Test.Matrix.Algebra
 where
 
-import qualified Prelude as P
-import Prelude hiding ( (+), (-), negate, subtract
-                      , (*), (/), recip, (^), (^^)
-                      , gcd
-                      , quotRem, quot, rem
-                      )
+import HLinear.Utility.Prelude
 
-import Data.Proxy
+import Data.Ratio ( Rational )
+
+import HLinear.Matrix.Sized
 import Math.Structure.Tasty
 import Test.Tasty
-
-import HLinear.Matrix
-import HLinear.Matrix.Sized
 
 
 properties :: TestTree
 properties =
   testGroup "Algebraic properties" $
     runTestsQC
-    [ isRing ( Proxy :: Proxy (MatrixSized 1 1 Rational) )
+    [ isAdditiveSemigroup ( Proxy :: Proxy (MatrixSized 3 5 Rational) )
+    , isRing ( Proxy :: Proxy (MatrixSized 1 1 Rational) )
     , isRing ( Proxy :: Proxy (MatrixSized 2 2 Rational) )
     , isRing ( Proxy :: Proxy (MatrixSized 3 3 Rational) )
     , isRing ( Proxy :: Proxy (MatrixSized 9 9 Rational) )
