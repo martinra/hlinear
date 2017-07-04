6d1e71aeafd6d8bd381ec9b4296eb10d79f28d72
Martin Raum <martin@raum-brothers.eu>
2017-07-04 18:53:54 +0200

    reorder matrix algebra tests

--- test/HLinear/Test/Matrix/Algebra.hs
+++ test/HLinear/Test/Matrix/Algebra.hs
@@ -16,14 +16,20 @@ import Test.Tasty
 
 properties :: TestTree
 properties =
-  testGroup "Algebraic properties" $
-    runTestsQC
-    [-- isAdditiveSemigroup ( Proxy :: Proxy (MatrixSized 3 5 Rational) )
-      isRing ( Proxy :: Proxy (MatrixSized 1 1 Rational) )
---    , isRing ( Proxy :: Proxy (MatrixSized 2 2 Rational) )
---    , isRing ( Proxy :: Proxy (MatrixSized 3 3 Rational) )
---    , isRing ( Proxy :: Proxy (MatrixSized 9 9 Rational) )
---    , isMultiplicativeLeftAction
---        ( Proxy :: Proxy ( MatrixSized 3 3 Rational ) )
---        ( Proxy :: Proxy ( MatrixSized 3 5 Rational ) )
+  testGroup "Algebraic properties"
+    [ testGroup "Matrices of size 3 x 5" $ runTestsQC
+      [ isAdditiveSemigroup ( Proxy :: Proxy (MatrixSized 3 5 Rational) ) ]
+    , testGroup "Matrices of size 1 x 1" $ runTestsQC
+      [ isRing ( Proxy :: Proxy (MatrixSized 1 1 Rational) ) ]
+    , testGroup "Matrices of size 2 x 2" $ runTestsQC
+      [ isRing ( Proxy :: Proxy (MatrixSized 2 2 Rational) ) ]
+    , testGroup "Matrices of size 3 x 3" $ runTestsQC
+      [ isRing ( Proxy :: Proxy (MatrixSized 3 3 Rational) ) ]
+    , testGroup "Matrices of size 9 x 9" $ runTestsQC
+      [ isRing ( Proxy :: Proxy (MatrixSized 9 9 Rational) ) ]
+    , testGroup "Matrices of size 3 x 3 acting on 3 x 5" $ runTestsQC
+      [ isMultiplicativeLeftAction
+          ( Proxy :: Proxy ( MatrixSized 3 3 Rational ) )
+          ( Proxy :: Proxy ( MatrixSized 3 5 Rational ) )
+      ]
     ]
