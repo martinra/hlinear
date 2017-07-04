e0e33d172d5ca2b3bd75ed82252916d2475659ef
Martin Raum <martin@raum-brothers.eu>
2017-07-04 21:05:41 +0200

    matrix tests use FMPQ

--- test/HLinear/Test/Matrix/Algebra.hs
+++ test/HLinear/Test/Matrix/Algebra.hs
@@ -7,8 +7,6 @@ where
 
 import HLinear.Utility.Prelude
 
-import Data.Ratio ( Rational )
-
 import HLinear.Matrix.Sized
 import Math.Structure.Tasty
 import Test.Tasty
@@ -18,18 +16,18 @@ properties :: TestTree
 properties =
   testGroup "Algebraic properties"
     [ testGroup "Matrices of size 3 x 5" $ runTestsQC
-      [ isAdditiveSemigroup ( Proxy :: Proxy (MatrixSized 3 5 Rational) ) ]
+      [ isAdditiveSemigroup ( Proxy :: Proxy (MatrixSized 3 5 FMPZ) ) ]
     , testGroup "Matrices of size 1 x 1" $ runTestsQC
-      [ isRing ( Proxy :: Proxy (MatrixSized 1 1 Rational) ) ]
+      [ isRing ( Proxy :: Proxy (MatrixSized 1 1 FMPQ) ) ]
     , testGroup "Matrices of size 2 x 2" $ runTestsQC
-      [ isRing ( Proxy :: Proxy (MatrixSized 2 2 Rational) ) ]
+      [ isRing ( Proxy :: Proxy (MatrixSized 2 2 FMPQ) ) ]
     , testGroup "Matrices of size 3 x 3" $ runTestsQC
-      [ isRing ( Proxy :: Proxy (MatrixSized 3 3 Rational) ) ]
+      [ isRing ( Proxy :: Proxy (MatrixSized 3 3 FMPQ) ) ]
     , testGroup "Matrices of size 9 x 9" $ runTestsQC
-      [ isRing ( Proxy :: Proxy (MatrixSized 9 9 Rational) ) ]
+      [ isRing ( Proxy :: Proxy (MatrixSized 9 9 FMPQ) ) ]
     , testGroup "Matrices of size 3 x 3 acting on 3 x 5" $ runTestsQC
       [ isMultiplicativeLeftAction
-          ( Proxy :: Proxy ( MatrixSized 3 3 Rational ) )
-          ( Proxy :: Proxy ( MatrixSized 3 5 Rational ) )
+          ( Proxy :: Proxy ( MatrixSized 3 3 FMPQ ) )
+          ( Proxy :: Proxy ( MatrixSized 3 5 FMPQ ) )
       ]
     ]
