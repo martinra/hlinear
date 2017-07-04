ad784f3be15297dd8489639e830884de953303a1
Martin Raum <martin@raum-brothers.eu>
2017-07-04 17:41:02 +0200

    deactivate tests

--- test/HLinear/Test/Matrix/Algebra.hs
+++ test/HLinear/Test/Matrix/Algebra.hs
@@ -18,12 +18,12 @@ properties :: TestTree
 properties =
   testGroup "Algebraic properties" $
     runTestsQC
-    [ isAdditiveSemigroup ( Proxy :: Proxy (MatrixSized 3 5 Rational) )
-    , isRing ( Proxy :: Proxy (MatrixSized 1 1 Rational) )
-    , isRing ( Proxy :: Proxy (MatrixSized 2 2 Rational) )
-    , isRing ( Proxy :: Proxy (MatrixSized 3 3 Rational) )
-    , isRing ( Proxy :: Proxy (MatrixSized 9 9 Rational) )
-    , isMultiplicativeLeftAction
-        ( Proxy :: Proxy ( MatrixSized 3 3 Rational ) )
-        ( Proxy :: Proxy ( MatrixSized 3 5 Rational ) )
+    [-- isAdditiveSemigroup ( Proxy :: Proxy (MatrixSized 3 5 Rational) )
+      isRing ( Proxy :: Proxy (MatrixSized 1 1 Rational) )
+--    , isRing ( Proxy :: Proxy (MatrixSized 2 2 Rational) )
+--    , isRing ( Proxy :: Proxy (MatrixSized 3 3 Rational) )
+--    , isRing ( Proxy :: Proxy (MatrixSized 9 9 Rational) )
+--    , isMultiplicativeLeftAction
+--        ( Proxy :: Proxy ( MatrixSized 3 3 Rational ) )
+--        ( Proxy :: Proxy ( MatrixSized 3 5 Rational ) )
     ]
