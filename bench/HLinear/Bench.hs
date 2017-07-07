9d4c3727a8d46bdbe963946f04927373e59d1033
Martin Raum <martin@raum-brothers.eu>
2017-07-07 15:03:02 +0200

    fix benchmark main

--- bench/HLinear/Bench.hs
+++ bench/HLinear/Bench.hs
@@ -8,45 +8,43 @@ where
 
 import HLinear.Utility.Prelude
 
+import Criterion.Main
+import Criterion.Types ( Config(..) )
 import Data.Attoparsec.ByteString.Char8 ( parseOnly )
 import Data.ByteString ( readFile )
-import Criterion.Main
-import Criterion.Types ( Config(..), Verbosity(..) )
 import Options.Applicative
 import System.FilePath ( FilePath )
 import System.IO ( IO, hPutStrLn, stderr )
+import qualified Criterion.Main.Options as Criterion
 import qualified System.FilePath as FP
 
-import HLinear.Bench.MatrixParser ( matrixFMPQParser )
+import HLinear.Bench.MatrixParser ( parserMatrixFMPQ )
 import HLinear.Matrix ( Matrix )
 import qualified HLinear.NormalForm.FoldUnfold.PLE.DivisionRing as PLEDR
 import qualified HLinear.NormalForm.FoldUnfold.PLE.FractionFree as PLEFF
 import qualified HLinear.NormalForm.FoldUnfold.ReduceEchelonForm.DivisionRing as RREF
 
 
-inputFileNameOption :: Parser String
-inputFileNameOption =
-  argument str (metavar "INPUT_FILENAME")
-
-
 main :: IO ()
 main =
-  let opts = info (inputFileNameOption <**> helper)
+  let opts = info
+               ( argument str (metavar "INPUT_FILENAME")
+            <**> helper )
                ( fullDesc
               <> progDesc "Benchmark HLinear for matrix in INPUT_FILENAME"
               <> header "bench-hlinear" )
   in do
     inputFileName <- execParser opts
     matrixString <- readFile inputFileName
-    case parseOnly matrixFMPQParser matrixString of
+    case parseOnly parserMatrixFMPQ matrixString of
       Left err -> hPutStrLn stderr $ "Parse error of '" <> inputFileName <> "': " <> err
       Right mat -> mainBenchmark inputFileName mat
 
 mainBenchmark :: FilePath -> Matrix FMPQ -> IO ()
 mainBenchmark fileName mat =
-  defaultMainWith
-  defaultConfig { jsonFile = Just $ fileName FP.<.> "json" }
-    [ bench "ple classical"     $ nf PLEDR.ple mat
-    , bench "ple fraction free" $ nf PLEFF.ple mat
-    , bench "rref" $ nf RREF.rref mat
-    ]
+  let cfg = defaultConfig { jsonFile = Just $ fileName FP.<.> "json" }
+  in  runMode (Criterion.Run cfg Criterion.Prefix [])
+        [ bench "ple classical"     $ nf PLEDR.ple mat
+        , bench "ple fraction free" $ nf PLEFF.ple mat
+        , bench "rref" $ nf RREF.rref mat
+        ]
