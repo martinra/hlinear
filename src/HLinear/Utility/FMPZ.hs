46725e8882b55c35677369c29b55b89f2cee47d6
Martin Raum <martin@raum-brothers.eu>
2017-07-30 22:06:32 +0200

    inline FMPZ functions

--- src/HLinear/Utility/FMPZ.hs
+++ src/HLinear/Utility/FMPZ.hs
@@ -7,6 +7,7 @@ import HFlint.FMPZ.FFI ( fmpz_set, fmpz_mul, fmpz_submul, fmpz_divexact )
 import HFlint.Internal ( withFlint_, withNewFlint_ )
 
 
+{-# INLINE subMul #-}
 subMul
   :: FMPZ -> FMPZ -> FMPZ -> FMPZ
 subMul a b c = unsafePerformIO $
@@ -17,6 +18,7 @@ subMul a b c = unsafePerformIO $
     fmpz_set rptr aptr
     fmpz_submul rptr bptr cptr
 
+{-# INLINE mulSubMul #-}
 mulSubMul
   :: FMPZ -> FMPZ -> FMPZ -> FMPZ -> FMPZ
 mulSubMul a b c d = unsafePerformIO $
@@ -28,6 +30,7 @@ mulSubMul a b c d = unsafePerformIO $
     fmpz_mul rptr aptr bptr
     fmpz_submul rptr cptr dptr
 
+{-# INLINE mulSubMulDivexact #-}
 mulSubMulDivexact
   :: FMPZ -> FMPZ -> FMPZ -> FMPZ -> FMPZ -> FMPZ
 mulSubMulDivexact a b c d e = unsafePerformIO $
